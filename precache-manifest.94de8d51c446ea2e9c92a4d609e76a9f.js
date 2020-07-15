e2f241a46e8557c2f549209d0c6ba9819aba5f75
Adria Pujol <adriapujolaparicio@gmail.com>
2020-07-15 22:16:41 +0200

    Updates

---
+++ precache-manifest.94de8d51c446ea2e9c92a4d609e76a9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a9845dd75e36cc6a664c644850903d",
+    "url": "/zlatanfacts/index.html"
+  },
+  {
+    "revision": "fdaf15f45fd4fe3aa083",
+    "url": "/zlatanfacts/static/css/main.599a7bb1.chunk.css"
+  },
+  {
+    "revision": "181208407a221e3ea01e",
+    "url": "/zlatanfacts/static/js/2.d872fa67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zlatanfacts/static/js/2.d872fa67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdaf15f45fd4fe3aa083",
+    "url": "/zlatanfacts/static/js/main.c2698c12.chunk.js"
+  },
+  {
+    "revision": "ffeb71a6da8795dc11e0",
+    "url": "/zlatanfacts/static/js/runtime-main.06acbbc4.js"
+  },
+  {
+    "revision": "852af8e68e8315df29139d3ed5668815",
+    "url": "/zlatanfacts/static/media/ibra.852af8e6.png"
+  },
+  {
+    "revision": "0e6b08eca8b84e11efdb6de963c870c1",
+    "url": "/zlatanfacts/static/media/ibra_logo.0e6b08ec.png"
+  }
+]);
